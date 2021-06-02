abf2a843f894344f6a3728d33fa0a264b8195100
anraman1 <anand_india_tn@yahoo.co.in>
2021-06-01 21:34:11 -0700

    api python

---
+++ Documents/RD/apipython.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import requests
+response = requests.get('https://192.168.33.40/api/v2/job_templates/', verify=False, headers={"Accept":"Application/json", "Authorization":"Bearer ie8tGeJodYWSrrQl2UeC9lc7l1SvZL"})
+
+responseoutput = response.json()
+
+print ("Template name\n")
+for i in range(len(response.json()['results'])):
+  print (responseoutput['results'][i]['name'])
+
+
