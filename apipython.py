00c3fdfd8927f892439d134e93201caafd5a314a
andr <you@example.com>
2021-09-30 00:35:15 +0000

    para

--- apipython.py
+++ apipython.py
@@ -6,6 +6,10 @@ import sys
 tower_url='https://192.168.33.40/api/v2/job_templates/?name__contains={}'.format(sys.argv[1])
 print (tower_url)
 
+if sys.argv[1] == 'all':
+   tower_url='https://192.168.33.40/api/v2/job_templates/'
+
+
 response = requests.get(tower_url, verify=False, headers={"Accept":"Application/json", "Authorization":"Bearer FFsnDGoskDHA3clUVERONbOEWXvJk6"})
 
 responseoutput = response.json()
