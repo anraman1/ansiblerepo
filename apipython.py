4744950a194f7ce097f8c83bb716826d338b6979
andr <you@example.com>
2021-09-30 00:26:10 +0000

    para

--- apipython.py
+++ apipython.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 #
 import requests
-response = requests.get('https://192.168.33.40/api/v2/job_templates/', verify=False, headers={"Accept":"Application/json", "Authorization":"Bearer FFsnDGoskDHA3clUVERONbOEWXvJk6"})
+import sys
+
+tower_url='https://192.168.33.40/api/v2/job_templates/?name__contains={}'.format(sys.argv[1])
+print (tower_url)
+
+response = requests.get(tower_url, verify=False, headers={"Accept":"Application/json", "Authorization":"Bearer FFsnDGoskDHA3clUVERONbOEWXvJk6"})
 
 responseoutput = response.json()
 
