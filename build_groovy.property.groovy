550c69259d752a8c29136a0919db1a051af4b53b
andr <you@example.com>
2021-10-02 14:29:19 +0000

    build_pro

---
+++ build_groovy.property.groovy
@@ -0,0 +1,17 @@
+properties([parameters([string(defaultValue: '', description: 'interger', name: 'inter', trim: false)])])
+
+ String string=params.inter
+ int loopint = Integer.parseInt(string);
+ 
+timeout(unit: 'SECONDS', time: 500) {
+    stage("One"){
+        node {
+            sleep 2
+            echo params.inter
+            for (int i = 0; i<loopint; i++){
+                   echo "Hello"
+            }
+            
+    }
+}
+}
